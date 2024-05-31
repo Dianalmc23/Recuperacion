fbdde7d9e8cccaedf8c1c92eb1814639212f1231
diana <dianalauramatacanchola@gmail.com>
2024-05-31 12:20:25 -0600

    ⭐

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import FancyButton from './components/FancyButton.vue';
+</script>
+
+<template>
+  <div class="content">
+  
+    <FancyButton>
+      <template v-slot:icon="slotProps">
+     {{ slotProps.hover ? '' : '' }}
+      </template>
+      <template v-slot:default>
+      <strong>New software update available</strong>
+    </template>
+    </FancyButton>
+
+    <FancyButton>
+      <template v-slot:icon="slotProps">
+     {{ slotProps.hover ? '' : '' }}
+      </template>
+      <template v-slot:default>
+      <strong>Your Purchase has been cofirmed</strong>
+    </template>
+    </FancyButton>
+
+    <FancyButton>
+      <template v-slot:icon="slotProps">
+     {{ slotProps.hover ? '' : '' }}
+      </template>
+      <template v-slot:default>
+      <strong>Warning: Invalid email address!</strong>
+    </template>
+    </FancyButton>
+
+    <FancyButton>
+      <template v-slot:icon="slotProps">
+     {{ slotProps.hover ? '' : '' }}
+      </template>
+      <template v-slot:default>
+      <strong>Error! Task failed successfully</strong>
+    </template>
+    </FancyButton>
+
+
+   
+  </div>
+</template>
+
+<style scoped>
+</style>
+  
+
+
+<style scoped>
+</style>
---
+++ src/components/FancyButton.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+const hover = ref(false);
+</script>
+
+<template>
+<header>
+    <div>
+    </div>
+  </header>
+
+  <div class="content">
+    <button 
+    @mouseenter="hover = true"
+    @mouseleave="hover = false">
+      <slot name="icon" :hover="hover"> ⚠ </slot>
+      <slot>No Message</slot>
+    </button>
+  
+  </div>
+</template>
+
+<style scoped>
+.content{
+  text-align: center;
+}
+
+button{
+  width: 30 rem;
+  margin: 30px auto;
+  font-size: 2rem;
+}
+</style>
